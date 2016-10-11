73d1173e634dedaa01a585b28ba76df4a886e288
danivegamx <ingdanivega@gmail.com>
2016-10-11 01:42:19 -0500

    First Component on React

---
+++ src/js/components/App.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+module.exports = React.createClass({
+
+    getInitialState(){
+        // Los return siempre regresan un objeto.
+        return {};
+    },
+
+    render() {
+        return (
+            <div>VUTD First React Component</div>
+        );
+    }
+
+});
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+var React = require('react'),
+	ReactDOM = require('react-dom'),
+	HomePage = require('./components/App');
+
+ReactDOM.render(<App title="VUTD" />, document.getElementById('react-container'));
